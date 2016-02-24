4a94673e2978018ebb747bfea6e40ef8f6695529
ysrotciv <ysrotciv@gmail.com>
2016-02-24 09:40:50 +0800

    inital commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "userportrait"
+
+version := "1.0"
+
+scalaVersion := "2.10.4"
+    
---
+++ src/main/scala/com/kunyan/userportrait/Extractor.scala
@@ -0,0 +1,8 @@
+package com.kunyan.userportrait
+
+/**
+  * Created by yangshuai on 2016/2/24.
+  */
+object Extractor {
+
+}
