00899c09c30e18895b61f519d0c67234e1ce4d37
zhangruibo <934398195@qq.com>
2016-02-25 13:30:57 +0800

    add extract rule of qq

--- src/main/scala/com/kunyan/userportrait/platform/Qzone.scala
+++ src/main/scala/com/kunyan/userportrait/platform/Qzone.scala
@@ -10,8 +10,10 @@ object Qzone extends  Platform {
   override val PLATFORM_NAME: String = "QQ"
   val urlListBuffer = new ListBuffer[String]()
   val QQListBuffer = new ListBuffer[String]()
+  var QQ = new String
   //个人中心
-  def PersonCenter(url:String):Unit={
+  def PersonCenter(url:String):Boolean={
+    var flag = false
     val personcenterone = "http://user.qzone.qq.com/\\d{1,}/infocenter?ptsig=".r
     val personcentertwo = "http://user.qzone.qq.com/\\d{1,}/infocenter?via=toolbar".r
     val resultone  = personcenterone.findAllMatchIn(url)
@@ -19,118 +21,183 @@ object Qzone extends  Platform {
     if(resultone != null) {
       resultone.foreach(x => {
         val QQone = url.split("/")(3)
-        urlListBuffer.+=(url)
-        QQListBuffer. +=(QQone)
+        flag =true
+        QQ = QQone
       })
     }else{
       if( resulttwo != null) {
         resulttwo.foreach(x => {
           val QQtwo = url.split("/")(3)
-          urlListBuffer.+=(url)
-          QQListBuffer. +=(QQtwo)
+          flag =true
+          QQ = QQtwo
         })
       }
     }
+    flag
   }
   //主页
-  def HomePage(url:String):Unit={
+  def HomePage(url:String):Boolean={
+    var flag = false
     val homepage = "http://user.qzone.qq.com/\\d{1,}/main".r
     val result  = homepage.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //相册
-  def PhotoAlbum(url:String):Unit={
+  def PhotoAlbum(url:String):Boolean={
+    var flag = false
     val photoalbum = "http://user.qzone.qq.com/\\d{1,}/4".r
     val result  = photoalbum.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //留言板
-  def MessageBoard(url:String):Unit={
+  def MessageBoard(url:String):Boolean={
+    var flag = false
     val messageboard = "http://user.qzone.qq.com/\\d{1,}/334".r
     val result  = messageboard.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //时光轴
-  def Time(url:String):Unit={
+  def Time(url:String):Boolean={
+    var flag = false
     val time = "http://user.qzone.qq.com/\\d{1,}/main?mode=gfp_timeline".r
     val result  = time.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //更多
-  def More(url:String):Unit={
+  def More(url:String):Boolean={
+    var flag = false
     val more = "http://user.qzone.qq.com/\\d{1,}/more".r
     val result  = more.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //个人档
-  def PersonalFile(url:String):Unit={
+  def PersonalFile(url:String):Boolean={
+    var flag = false
     val personalfile = "http://user.qzone.qq.com/\\d{1,}/profile".r
     val result  =  personalfile.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //好友
-  def Friend(url:String):Unit={
+  def Friend(url:String):Boolean={
+    var flag = false
     val friend = "http://user.qzone.qq.com/\\d{1,}/myhome/friends/center".r
     val result  =  friend.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //应用
-  def Application (url:String):Unit={
+  def Application (url:String):Boolean={
+    var flag = false
     val application  = "http://user.qzone.qq.com/\\d{1,}/appstore?via=qzoneframetop_nored".r
     val result  =  application .findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer. +=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
   //装扮
-  def Dress (url:String):Unit= {
+  def Dress (url:String):Boolean= {
+    var flag = false
     val dress = "http://user.qzone.qq.com/\\d{1,}/onekeydressup".r
     val result = dress.findAllMatchIn(url)
     result.foreach(x => {
-      val QQ = url.split("/")(3)
-      urlListBuffer.+=(url)
-      QQListBuffer.+=(QQ)
+      val QQzone = url.split("/")(3)
+      flag =true
+      QQ = QQzone
     })
+    flag
   }
     //空间设置
-    def InterSpace (url:String):Unit={
+    def InterSpace (url:String):Boolean={
+      var flag = false
       val inerspace  = "http://user.qzone.qq.com/\\d{1,}/profile/permit".r
       val result  =  inerspace .findAllMatchIn(url)
       result.foreach(x => {
-        val QQ = url.split("/")(3)
-        urlListBuffer.+=(url)
-        QQListBuffer. +=(QQ)
+        val QQzone = url.split("/")(3)
+        flag =true
+        QQ = QQzone
       })
+      flag
   }
 
-}
+  def QQzone(line:String): Unit ={
+    val  lineSplit = line.split("\t")
+    val ad = lineSplit(0)
+    val ua = lineSplit(1)
+    val url = lineSplit(2)
+    if(PersonCenter(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(HomePage(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(PhotoAlbum(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+  }else if(MessageBoard(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(Time(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(More(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(PersonalFile(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(Friend(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if(Application(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else if( Dress(url)){
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }else{
+      urlListBuffer.+=(line)
+      QQListBuffer. += (ad +"\t"+ua+"\t"+QQ)
+    }
+  }
+
+
+
+
+  }
--- src/main/scala/com/kunyan/userportrait/platform/WeiBo.scala
+++ src/main/scala/com/kunyan/userportrait/platform/WeiBo.scala
@@ -11,8 +11,48 @@ object WeiBo extends  Platform {
   val urlListBuffer = new ListBuffer[String]()
   //主页
   def HomePage(url:String):Unit={
-    val homepage = "http://user.qzone.qq.com/\\d{1,}/main".r
+    val homepage = "http://weibo.com/\\d{1,}/profile?".r
     val result  = homepage.findAllMatchIn(url)
+    result.foreach(x => {
+      urlListBuffer.+=(url)
+    })
+  }
+  //相册
+  def PhotoAlbum(url:String):Unit={
+    val photoalbum = "http://weibo.com/p/\\d{1,}/album?".r
+    val result  = photoalbum.findAllMatchIn(url)
+    result.foreach(x => {
+      urlListBuffer.+=(url)
+    })
+  }
+  //管理
+  def Manage(url:String):Unit={
+    val manage = "http://weibo.com/p/\\d{1,}/manage?".r
+    val result  = manage.findAllMatchIn(url)
+    result.foreach(x => {
+      urlListBuffer.+=(url)
+    })
+  }
+  //首页
+  def Home(url:String):Unit={
+    val home = "http://weibo.com/p/\\d{1,}/manage?".r
+    val result  =home.findAllMatchIn(url)
+    result.foreach(x => {
+      urlListBuffer.+=(url)
+    })
+  }
+  //发现
+  def Found(url:String):Unit={
+    val found = "http://d.weibo.com/\\d{6}".r
+    val result  =found.findAllMatchIn(url)
+    result.foreach(x => {
+      urlListBuffer.+=(url)
+    })
+  }
+  //个人信息
+  def PersonInformation(url:String):Unit={
+    val personinformation = "http://weibo.com/p/\\d{6}/nfo?mod=pedit".r
+    val result  =personinformation.findAllMatchIn(url)
     result.foreach(x => {
       urlListBuffer.+=(url)
     })
