76e0e905529ac830e7fc6d04f1854149e26620f1
lcm <liaochengming@kunyan-inc.com>
2016-03-31 16:11:18 +0800

    3.31

--- src/main/scala/com/kunyan/userportrait/temp/lcm/FilterAd.scala
+++ src/main/scala/com/kunyan/userportrait/temp/lcm/FilterAd.scala
@@ -8,7 +8,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 /**
  * Created by lcm on 2016/3/23.
  */
-object FilterAd extends Serializable {
+object FilterAd {
 
   val setStockAd = new scala.collection.mutable.TreeSet[String]()
   val setRichAd = new scala.collection.mutable.TreeSet[String]()
@@ -17,27 +17,29 @@ object FilterAd extends Serializable {
   def main(args: Array[String]) {
 
     if (args.length == 4) {
-      val srcIn = args(0) //+ getDay + "/" + "*"
+      val srcIn = args(0) + getDay + "/" + "*"
       val StockAdOut = args(1)
       val RichAdOut = args(2)
       val MergeAdOut = args(3)
 
-      val sparkConf = new SparkConf().setAppName("FILTER AD").setMaster("local")
+      val sparkConf = new SparkConf().setAppName("FILTER AD")
       val sc = new SparkContext(sparkConf)
+
       //输入的数据文件
       val data = sc.textFile("file://" + srcIn)
-      //输入的ad文件
+
+      //以前的股票用户ad和访问网站次数
       val fileStockAd = sc.textFile("file://" + StockAdOut).filter(_.split("\t").length == 2).map(x => {
         val arr = x.split("\t")
         (arr(0), arr(1).toInt)
       })
-
+      //以前的奢侈品用户ad和访问网站次数
       val fileRichAd = sc.textFile("file://" + RichAdOut).filter(_.split("\t").length == 2).map(x => {
         val arr = x.split("\t")
         (arr(0), arr(1).toInt)
       })
 
-      //过滤出访问了股票网站的用户
+      //过滤出新一天访问了股票用户的ad和访问网站次数
       val stockAdRdd = data.map(x => x.split("\t"))
         .filter(_.size > 3)
         .filter(!_ (1).contains("."))
@@ -45,12 +47,12 @@ object FilterAd extends Serializable {
         .map(x => (x(1), 1))
         .groupByKey()
         .map(x => (x._1, x._2.size))
+
+      //合并股票用户的ad和访问网站次数
       stockAdRdd.fullOuterJoin(fileStockAd).filter(x => x._1 != null).map(x => {
         if (x._2._2 == None) {
-          println(x)
           x._1 + "\t" + x._2._1.get
         } else if (x._2._1 == None) {
-          println(x)
           x._1 + "\t" + x._2._2.get
         } else {
           x._1 + "\t" + (x._2._1.get + x._2._2.get)
@@ -59,18 +61,17 @@ object FilterAd extends Serializable {
         setStockAd.+=(x)
       })
 
-      //过滤访问了奢侈品的网站的用户
+      //过滤出新一天奢侈品用户的ad和访问网站次数
       val richAdRdd = data.map(x => x.split("\t"))
         .filter(_.size > 3)
         .filter(filterUrl2)
         .filter(!_ (1).contains(".")).map(x => (x(1), 1)).groupByKey().map(x => (x._1, x._2.size)) //.saveAsTextFile("C:\\Users\\lcm\\Documents\\new_rich_ad")
-      //合并新旧ad和次数
+
+      //合并奢侈品用户的ad和访问网站次数
       richAdRdd.fullOuterJoin(fileRichAd).map(x => {
         if (x._2._2 == None) {
-          println(x)
           x._1 + "\t" + x._2._1.get
         } else if (x._2._1 == None) {
-          println(x)
           x._1 + "\t" + x._2._2.get
         } else {
           x._1 + "\t" + (x._2._1.get + x._2._2.get)
@@ -102,7 +103,7 @@ object FilterAd extends Serializable {
       val rdd2 = newFileRichAd.map(x => x.split("\t")).filter(_.length == 2).map(x => (x(0), x(1).toInt))
       //合并相同的ad
       val rdd = rdd1.join(rdd2).map(x => {
-          x._1 + "\t" + (x._2._1 + x._2._2)
+        x._1 + "\t" + (x._2._1 + x._2._2)
       })
 
       rdd.foreach(x => {
@@ -118,8 +119,8 @@ object FilterAd extends Serializable {
     }
   }
 
+  //判断是否访问过股票网站
   def filterUrl1(arrs: Array[String]): Boolean = {
-    //println(data)
     //股票网站的url字符串
     val strStockUrl = "http://finance.china.com.cn/,http://finance.sina.com.cn/,http://www.caijing.com.cn/,http://www.ce.cn/,http://www.17ok.com/,http://www.jrj.com.cn/,http://finance.qq.com/,http://www.eastmoney.com/,http://www.caixin.com/,http://money.163.com/,http://finance.ifeng.com/,http://business.sohu.com/,http://xueqiu.com/,http://www.10jqka.com.cn/,http://www.cnfol.com/,http://www.hexun.com/,http://www.cailianpress.com/,http://live.sina.com.cn/zt/f/v/finance/globalnews1,http://guba.eastmoney.com/,http://guba.sina.com.cn/,http://finance.china.com.cn/,http://finance.sina.com.cn/,http://www.caijing.com.cn/,http://www.ce.cn/,http://www.17ok.com/,http://www.jrj.com.cn/,http://finance.qq.com/,http://www.eastmoney.com/,http://www.caixin.com/,http://money.163.com/,http://finance.ifeng.com/,http://business.sohu.com/,http://xueqiu.com/,http://www.10jqka.com.cn/,http://www.cnfol.com/,http://www.hexun.com/,http://www.simuwang.com,http://www.howbuy.com,http://www.licai.com,http://www.go-goal.com,https://www.touzi.com,http://www.zhongguocaifu.com.cn,http://www.jfz.com,http://simu.eastmoney.com/"
     val urls = arrs(3).split("/")
@@ -132,13 +133,11 @@ object FilterAd extends Serializable {
     return false
   }
 
-
+  //判断是否访问过奢侈品网站
   def filterUrl2(data: Array[String]): Boolean = {
-    //println(data)
     //奢侈品和汽车的url字符串
     val strCarUrl = "http://www.chanel.com/,http://www.louisvuitton.cn/,http://www.dior.cn/,http://www.versace.com/,http://www.prada.com/,http://www.sephora.cn/,http://www.valentino.cn/,http://www.hugoboss.cn/,http://lesailes.hermes.com/,https://cn.burberry.com/,https://www.kenzo.com/,http://www.givenchy.com/,http://www.cartier.cn/,http://www.tiffany.cn/,http://www.bulgari.com/,http://cn.vancleefarpels.com/,http://www.harrywinston.com/,http://www.darryring.com/,http://www.damiani.com/,http://cn.boucheron.com/,http://www.mikimoto.com.hk/,http://www.swarovski.com.cn/,http://www.gucci.com/,http://www.prada.com/,http://www.armani.cn/,https://www.dunhill.cn/,http://www.fendi.cn/,http://china.coach.com/,http://www.louisvuitton.com/,http://www.chanel.com/,http://www.dior.cn/,http://www.valentino.cn/,http://www.patek.com/,http://www.audemarspiguet.com/,http://www.piaget.cn/,http://www.jaeger-lecoultre.com/,http://www.vacheron-constantin.com/,http://www.rolex.com/,http://www.iwc.com/,http://www.girard-perregaux.ch/,http://www.omegawatches.cn/,http://www.rolls-roycemotorcars.com.cn/,http://www.bmw.com.cn/,http://www.chrysler.com.cn/,http://www.audi.cn/,http://www.hdmi.org/,http://www.ford.com.cn/,http://www.maserati.com.cn/,http://www.bentleymotors.com/,http://www.daimler.com/,www.mercedes-benz.com.cn,http://www.prada.com/,http://www.oakley.com.cn/,http://judithleiber.com/,http://www.donnakaran.com/,http://www.ysl.com/,http://www.chanel.com/,http://www.dior.cn/,http://www.cartier.cn/,http://www.louisvuitton.cn/,http://www.parkerpen.com/,http://www.montblanc.cn/,http://www.waterman.com/,http://www.tw.cartier.com/,http://www.sheaffer.com/,http://www.aurorapen.it/,http://www.cross.com/,http://www.montegrappa.com/,http://www.hennessy.com/,http://www.absolut.com/,https://www.johnniewalker.com/,http://www.chivas.com.cn/,http://cn.moet.com/,http://www.remymartin.com/,http://www.martell.com/,http://www.bacardi.com/,https://www.malts.com/,https://www.johnniewalker.com.cn/,http://bijan.com/,http://www.tiffany.cn/,http://www.opiumbarcelona.com/,https://www.joythestore.com/,http://www.chanel.com/,http://www.hermes.com/,http://www.dunloptires.com/,http://taylormadegolf.com/,http://www.nike.com/,http://www.adidas.com.sg/,http://www.benhoganmuseum.org/,http://www.etonic.com/,http://www.golfsmith.com/,http://www.callawaygolf.cn/,http://www.chinapinggolf.com/,http://www.chanel.com/,http://www.esteelauder.com.cn/,http://www.lancome.com.cn/,http://www.dior.cn/,http://www.guerlain.com.cn/,https://www.shiseido.com.hk/,http://china.elizabetharden.com/,http://www.avon.com.cn/,http://www.marykay.com/,http://www.cartier.cn/,http://www.tiffany.cn/,http://www.bulgari.com/,http://cn.vancleefarpels.com/,http://www.harrywinston.com/,http://www.derier.com.cn/,http://www.damiani.com/,http://cn.boucheron.com/,http://www.mikimoto.com.hk/,http://www.swarovski.com.cn/,http://www.autohome.com.cn/"
     val urls = data(3).split("/")
-    //println(data(3))
     if (urls.length > 0) {
       val url = if (urls(0) == "http:" || urls(0) == "https:") urls(2) else urls(0)
       if (strCarUrl.contains(url)) {
--- src/main/scala/com/kunyan/userportrait/temp/lcm/RichManInfo.scala
+++ src/main/scala/com/kunyan/userportrait/temp/lcm/RichManInfo.scala
@@ -1,165 +1,170 @@
 import java.io.{File, PrintWriter}
 
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Created by lcm on 2016/3/28.
  */
 object RichManInfo {
 
-  val setElemePhone = new scala.collection.mutable.TreeSet[String]()
-  val setSuNingPhone = new scala.collection.mutable.TreeSet[String]()
-  val setWeiBoPhone = new scala.collection.mutable.TreeSet[String]()
-  val setEmail = new scala.collection.mutable.TreeSet[String]()
-  val setQq = new scala.collection.mutable.TreeSet[String]()
-  val setRichInfo = new scala.collection.mutable.TreeSet[String]()
+  val setAllPhone = new scala.collection.mutable.TreeSet[String]()
+  val listRichInfo = new scala.collection.mutable.ListBuffer[String]()
 
   def main(args: Array[String]) {
-    val file_in = args(0)
-    val file_out = args(1)
-    //val sparkConf = new SparkConf().setAppName("RICH MAN INFO").setMaster("local")
     val sparkConf = new SparkConf().setAppName("RICH MAN INFO")
+    //val sparkConf = new SparkConf().setAppName("RICH MAN INFO")
     val sc = new SparkContext(sparkConf)
-
+    //将用户的所有手机号码输出到文件
+    val writerRichPhone = new PrintWriter(new File("F:\\资料\\hadoop\\rich_info\\rich_phone"), "UTF-8")
     //输出的股票用户rich_info
-    //val writerRichInfo = new PrintWriter(new File("F:\\资料\\hadoop\\rich_info\\rich_info"), "UTF-8")
-    val writerRichInfo = new PrintWriter(new File(file_out), "UTF-8")
+    val writerRichInfo = new PrintWriter(new File("F:\\资料\\hadoop\\rich_info\\rich_info"), "UTF-8")
 
     //将饿了么手机号数据读取到set集合
-    val eleme_phone = sc.textFile("file://" + file_in + "rich_phone/eleme_phone/*").map(x => {
-      //val eleme_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\eleme_phone\\*").map(x => {
+    //val eleme_phone = sc.textFile("file://" + file_in + "rich_phone/eleme_phone/*").map(x => {
+    val eleme_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\eleme_phone\\*").map(x => {
       val arr = x.split("\t")
       if (arr.length == 3) {
-        arr(0) + "\t" + arr(2)
+        (arr(0) + "\t" + arr(2))
       } else {
         null
       }
-    }).filter(x => x != null)
-      .foreach(x => {
-        setElemePhone.+=(x)
-      })
+    }).filter(x => x != null).foreach(x => setAllPhone.+=(x))
+
     //将suning手机号数据读取到set集合
-    val suning_phone = sc.textFile("file://" + file_in + "rich_phone/suning_phone/*").map(x => {
-      //val suning_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\suning_phone\\*").map(x => {
+    //val suning_phone = sc.textFile("file://" + file_in + "rich_phone/suning_phone/*").map(x => {
+    val suning_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\suning_phone\\*").map(x => {
       val arr = x.split("\t")
       if (arr.length > 3) {
-        arr(1) + "\t" + arr(3)
+        (arr(1) + "\t" + arr(3))
       } else {
         null
       }
-    }).filter(x => x != null)
-      .foreach(x => {
-        setSuNingPhone.+=(x)
-      })
+    }).filter(x => x != null).foreach(x => setAllPhone.+=(x))
+
     //将weibo手机号数据读取到set集合
-    val weibo_phone = sc.textFile("file://" + file_in + "rich_phone/weibo_phone/*").map(x => {
-      //val weibo_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\weibo_phone\\*").map(x => {
+    //val weibo_phone = sc.textFile("file://" + file_in + "rich_phone/weibo_phone/*").map(x => {
+    val weibo_phone = sc.textFile("F:\\资料\\hadoop\\rich_info\\phone\\weibo_phone\\*").map(x => {
       val arr = x.split("\t")
       if (arr.length > 3) {
-        arr(1) + "\t" + arr(3)
+        (arr(1) + "\t" + arr(3))
       } else {
         null
       }
-    }).filter(x => x != null)
-      .foreach(x => {
-        setWeiBoPhone.+=(x)
-      })
+    }).filter(x => x != null).foreach(x => setAllPhone.+=(x))
+
 
     //将email读取到Set
-    val email = sc.textFile("file://" + file_in + "rich_email/*")
-    //val email = sc.textFile("F:\\资料\\hadoop\\rich_info\\qq\\email\\*")
+    //val email = sc.textFile("file://" + file_in + "rich_email/*")
+    val email = sc.textFile("F:\\资料\\hadoop\\rich_info\\qq\\email\\*")
       .map(x => {
         val arr = x.split("\t")
         if (arr.length == 2) {
-          arr(0) + "\t" + arr(1)
+          (arr(0), arr(1))
         } else {
           null
         }
-      }).filter(x => x != null)
-      .foreach(x => {
-        setEmail.+=(x)
-      })
+      }).filter(x => x != null).groupByKey()
 
     //将qq读取到set
-    val qq = sc.textFile("file://" + file_in + "rich_qq/*")
-    //val qq = sc.textFile("F:\\资料\\hadoop\\rich_info\\qq\\qq\\*")
+    //val qq = sc.textFile("file://" + file_in + "rich_qq/*")
+    val qq = sc.textFile("F:\\资料\\hadoop\\rich_info\\qq\\qq\\*")
       .map(x => {
         val arr = x.split("\t")
         if (arr.length == 2) {
-          arr(0) + "\t" + arr(1)
+          (arr(0), arr(1))
         } else {
           null
         }
-      }).filter(x => x != null)
-      .foreach(x => {
-        setQq.+=(x)
-      })
-    sc.textFile("file://" + file_in + "rich_ad/merge_ad")
-    //sc.textFile("F:\\资料\\hadoop\\rich_info\\ad\\merge_ad")
-          .map(x => {
-            var str = x
-            for (e <- setElemePhone) {
-              val arr = e.split("\t")
-              if (x == arr(0)) {
-                str = str + "," + arr(1)
-              }
-            }
-            for (s <- setSuNingPhone) {
-              val arr = s.split("\t")
-              if (x == arr(0)) {
-                str = str + "," + arr(1)
-              }
-            }
-            for (w <- setWeiBoPhone) {
-              val arr = w.split("\t")
-              if (x == arr(0)) {
-                str = str + "," + arr(1)
-              }
-            }
-            str
-          }).map(x => {
-      if (x.contains(",")) {
-        x.replaceFirst(",", "\t")
-      } else {
-        //println(x + "\t" + "null")
-        x + "\t" + "null"
-      }
-    }).map(x => {
-      var str = x
-      for (e <- setEmail) {
-        val arr = e.split("\t")
-        if (x.split("\t")(0) == arr(0)) {
-          str = str + "\t" + arr(1)
-        }
-      }
-      str
-    }).map(x => {
-      var str = x
-      val arrData = x.split("\t")
-      if (arrData.length == 2) {
-        for (q <- setQq) {
-          val arrQQ = q.split("\t")
-          if (arrData(0) == arrQQ(0)) {
-            str = str + "\t" + arrQQ(1) + "@qq.com"
-            //println("str  =  " + str)
-          }
-        }
-      }
-      str
-    }).filter(x => {
-      val arr = x.split("\t")
-      !(arr.length == 2 && arr(1) == "null")
-    }).map(formatData)
-      .foreach(x => {
-        setRichInfo.+=(x)
-        println(x)
-      })
+      }).filter(x => x != null).groupByKey()
 
-    for (x <- setRichInfo) {
-      writerRichInfo.write(x)
+    //sc.textFile("file://" + file_in + "rich_ad/merge_ad")
+    val ad = sc.textFile("F:\\资料\\hadoop\\rich_info\\ad\\merge_ad").map(x => {
+      val arr = x.split("\t")
+      (arr(0), arr(1))
+    })
+
+    for (x <- setAllPhone) {
+      writerRichPhone.write(x + "\n")
+    }
+    writerRichPhone.close()
+
+    val AllPhoneRdd = sc.textFile("F:\\资料\\hadoop\\rich_info\\rich_phone").map(x => {
+      val arr = x.split("\t")
+      if (arr.length == 2) {
+        (arr(0), arr(1))
+      } else {
+        null
+      }
+    }).filter(x => x != null).groupByKey()
+    //匹配出用户的手机号码
+    val ad_phone = ad.leftOuterJoin(AllPhoneRdd)
+      .map(x => {
+        var str = ""
+        if (x._2._2 == None) {
+          x._1 + "\t" + x._2._1 + "\t" + "null"
+        } else {
+          x._2._2.foreach(i => {
+            i.foreach(j => {
+              str = str + "," + j
+            })
+          })
+          x._1 + "\t" + x._2._1 + "\t" + str.replaceFirst(",", "")
+        }
+      }).map(x => {
+      val arr = x.split("\t")
+      (arr(0), arr(1) + "\t" + arr(2))
+    })
+    //匹配出手email
+    val ad_phone_email = ad_phone.leftOuterJoin(email)
+      .map(x => {
+        var str = ""
+        if (x._2._2 == None) {
+          x._1 + "\t" + x._2._1 + "\t" + "null"
+        } else {
+          x._2._2.foreach(i => {
+            i.foreach(j => {
+              str = str + "," + j
+            })
+          })
+          x._1 + "\t" + x._2._1 + "\t" + str.replaceFirst(",", "")
+        }
+      })
+    //匹配QQ号
+    ad_phone_email.map(x => {
+      val arr = x.split("\t")
+      (arr(0), arr(1) + "\t" + arr(2) + "\t" + arr(3))
+    }).leftOuterJoin(qq)
+      .map(x => {
+        var str = ""
+        val arr = x._2._1.split("\t")
+        if (arr(2) == "null") {
+          x._2._2.foreach(i => {
+            i.foreach(j => {
+              str = str + "," + j + "@qq.com"
+            })
+          })
+          if (str == "") {
+            x._1 + "\t" + arr(0) + "\t" + arr(1) + "\t" + "null"
+          } else {
+            x._1 + "\t" + arr(0) + "\t" + arr(1) + "\t" + str.replaceFirst(",", "")
+          }
+        } else {
+          x._1 + "\t" + arr(0) + "\t" + arr(1) + "\t" + arr(2)
+        }
+      })
+      .filter(x => {
+        val arr = x.split("\t")
+        //arr.length < 4
+        !(arr(2) == "null" && arr(3) == "null")
+      }).sortBy(x =>x.split("\t")(1).toInt,false)
+      .foreach(x => listRichInfo.+=(x))
+
+    for (x <- listRichInfo) {
+      writerRichInfo.write(x + "\n")
     }
     writerRichInfo.close()
   }
+
 
   //格式化数据
   def formatData(data: String): String = {
