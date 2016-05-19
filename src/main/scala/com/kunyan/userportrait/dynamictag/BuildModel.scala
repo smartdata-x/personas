ecf3b420021780e8646d70ec494f1cc626fc8a96
Christph-cc <1057042131@qq.com>
2016-05-19 13:55:46 +0800

    modify the code of dynamictag

--- src/main/scala/com/kunyan/userportrait/dynamictag/BuildModel.scala
+++ src/main/scala/com/kunyan/userportrait/dynamictag/BuildModel.scala
@@ -7,8 +7,9 @@ import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.{SparkContext, SparkConf}
 
 /**
-  * Created by Administrator on 2016/4/20.
+  * Created by wangcao on 2016/4/20.
   *
+  * 构建分类模型
   * features:点击股票类url的总次数，点击股票类url的总个数，refernce为股票类url的总次数，reference为股票类url的总个数
   * compare model: logistic regression model; SVM model; bayes model
   * finally used the bayes model
@@ -21,7 +22,7 @@ object BuildModel {
     val conf = new SparkConf().setAppName("Target").setMaster("local")
     val sc = new SparkContext(conf)
 
-    // input the training data
+    // import and pre-process the training data
     val input = sc.textFile(args(0)).filter(x => !x.contains("()"))
 
     val parsedData = input.map { line =>
@@ -33,67 +34,60 @@ object BuildModel {
     val train = splits(0).cache()
     val test = splits(1).cache()
 
-
-    /** **
-      * 1 logistic regression model
-      * ***/
+    //1 logistic regression model
     val modelLogistic = LogisticRegressionWithSGD.train(train, 200)
     modelLogistic.save(sc, args(1))
 
     val predictionAndLabels1 = test.map {
+
       case LabeledPoint(label, features) =>
         val prediction = modelLogistic.predict(features)
+
         (prediction, label)
     }
-    val metrics1 = new MulticlassMetrics(predictionAndLabels1)
-    val precision1 = metrics1.precision(1.0)
-    val recall1 = metrics1.recall(1.0)
+    val metricsLogistic = new MulticlassMetrics(predictionAndLabels1)
+    val precisionLogistic = metricsLogistic.precision(1.0)
+    val recallLogistic = metricsLogistic.recall(1.0)
 
-    /** **
-      * 2 SVM model
-      * ***/
+    //2 SVM model
     val modelSVM = SVMWithSGD.train(train, 200)
     modelSVM.save(sc, args(2))
 
     val predictionAndLabels2 = test.map {
+
       case LabeledPoint(label, features) =>
         val prediction = modelSVM.predict(features)
+
         (prediction, label)
     }
 
-    val metrics2 = new MulticlassMetrics(predictionAndLabels2)
-    val recall2 = metrics2.recall(1.0)
-    val precision2 = metrics2.precision(1.0)
+    val metricsSVM = new MulticlassMetrics(predictionAndLabels2)
+    val recallSVM = metricsSVM.recall(1.0)
+    val precisionSVM = metricsSVM.precision(1.0)
 
 
-    /** ***
-      * 3 NaiveBayes Model
-      * ***/
+    //3 NaiveBayes Model
     val modelBayes = NaiveBayes.train(train)
     modelBayes.save(sc, args(3))
 
     val predictionAndLabels3 = test.map {
+
       case LabeledPoint(label, features) =>
         val prediction = modelBayes.predict(features)
+
         (prediction, label)
     }
 
-    val metrics3 = new MulticlassMetrics(predictionAndLabels3)
-    val recall3 = metrics3.recall(1.0)
-    val precision3 = metrics3.precision(1.0)
+    val metricsBayes = new MulticlassMetrics(predictionAndLabels3)
+    val recallBayes = metricsBayes.recall(1.0)
+    val precisionBayes = metricsBayes.precision(1.0)
+
+    println("logistic:" + recallLogistic +"/t" + precisionLogistic + "\n" +
+      "SVM" + recallSVM + "\t" + precisionSVM  + "\n" +
+      "Bayes:" + recallBayes +"\t" + precisionBayes)
 
     sc.stop()
 
   }
-
-//        //the function of scale-down features
-//        def toStandard (vec:RDD[String]): RDD[Double] = {
-//          val rdd = vec.map(_.split(" ").map(_.toDouble)).map(line=>Vectors.dense(line))
-//          val summary = Statistics.colStats(rdd)
-//          val min = summary.min(0)
-//          val max = summary.max(0)
-//          val standardVec = vec.map(x => (x.toDouble - min)/(max - min))
-//          standardVec
-//        }
 
 }
--- src/main/scala/com/kunyan/userportrait/dynamictag/TargetFeatures.scala
+++ src/main/scala/com/kunyan/userportrait/dynamictag/TargetFeatures.scala
@@ -5,44 +5,74 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 /**
   * Created by wangcao on 2016/4/15.
+  *
   * target:统计每个AD,或IP的访问网页的行为（奢侈品网站的访问来判定是否有钱；股票网站的访问来判定是否玩股票）
   * 统计指标用于做分类的features
   */
 object TargetFeatures {
 
-  def getUrlRemoveHead(url:String):String= {
+  /**
+    * 去掉url的http://或者https://
+    *
+    * @param url 用户访问的url或者reference
+    * @return 规格化的url
+    */
+  def getUrlRemoveHead(url: String): String= {
+
     var part = url
     try {
+
       if (url.contains("http://")) {
-        part = url.replace("http://", "").split("/").filter(x => x.size > 0)(0)
+        part = url.replace("http://", "").split("/").filter(x => x.length > 0)(0)
       } else if (url.contains("https://")) {
-        part = url.replace("https://", "").split("/").filter(x => x.size > 0)(0)
+        part = url.replace("https://", "").split("/").filter(x => x.length > 0)(0)
       } else {
-        part = url.split("/").filter(x => x.size > 0)(0)
+        part = url.split("/").filter(x => x.length > 0)(0)
       }
+
     } catch {
-      case e:Exception =>
+      case e: Exception =>
     }
+
     part
   }
 
-  def getUrlBody(url:String):String= {
+  /**
+    * 取出url的主域名
+    *
+    * @param url 去掉http://部分的Url
+    * @return 输出运算需要的url格式
+    */
+  def getUrlBody(url: String): String = {
+
     val host = getUrlRemoveHead(url)
     var part = host
+
     try{
+
       if (host.startsWith("www.")){
         part = host.replace("www.","")
       } else if (!host.contains("www")){
         part = host
       }
+
     } catch {
-      case e:Exception =>
+      case e: Exception =>
     }
+
     part
   }
 
-  def getUrlKey(url:String):String={
+  /**
+    * 调整目标url的格式
+    *
+    * @param url 奢侈品或者股票网链接
+    * @return 调整好格式的奢侈品或股票网链接
+    */
+  def getUrlKey(url:  String): String = {
+
     var part = url
+
     if (url.contains("http://www")){
       part = url.substring(11,url.length)
     } else if (url.contains("https://www")){
@@ -50,27 +80,32 @@ object TargetFeatures {
     } else {
       part = url
     }
+
     part
   }
 
+  /**
+    * 主程序main方法
+    *
+    * @param args
+    */
   def main(args:Array[String]): Unit ={
 
-    val conf = new SparkConf().setAppName("TargetFeatures")//.setMaster("local")
+    val conf = new SparkConf().setAppName("TargetFeatures")
     val sc = new SparkContext(conf)
 
-    /**
-      * 1. input and pre-process data
-      */
+    //1. input and pre-process data
     //1.1 input total data from telecom
     val data = sc.textFile(args(0))
       .map(_.split("\t")).filter(x => x.size == 6)
-      .map(x =>(x(1),x(2),getUrlBody(x(3)),getUrlBody(x(4))))
+      .map(x =>(x(1), x(2), getUrlBody(x(3)), getUrlBody(x(4))))
       .filter(x => !x._2.contains("spider") && !x._2.contains("Spider"))
       .filter(x => x._3 != "qq.com" && !x._3.contains("mail") && x._3 !="ip.com" && x._3 != "baidu.com" && x._3 != "so.com" && x._3 != "ti.com.cn" && x._3 != "le.com" && x._3 != "sina.com.cn" )
+
     data.persist(MEMORY_AND_DISK)
 
     //1.2 input target url (including both original url and extra url)
-    val totalUrl = sc.textFile("args(1)")
+    val totalUrl = sc.textFile(args(1))
       .map(x => x.trim)
       .map(x => getUrlKey(x))
       .distinct()
@@ -78,53 +113,48 @@ object TargetFeatures {
       .collect.mkString(",", ",", ",")
 
 
-    /**
-      * 2. exploring the target url
-      */
-
+    //2. exploring the target url
     //2.1 label the target url as 1 and cache the outputted records
     val labelLineRddUr = data.filter(x => totalUrl.contains(x._3)).map(x=>{
+
       val url = x._3
       val domain = url
       (x._1 +"\t"+x._2+ "\t" + domain, 1)
+
     }).reduceByKey(_ + _).sortBy(x=>x._2,false)
 
     labelLineRddUr.persist(MEMORY_AND_DISK)
 
     //2.2 calculate how many times each Ad visits the target url and how many distinct target url each Ad visits
     val visitNumUr = labelLineRddUr.map(x => (x._1.split("\t")(0), x._2)).reduceByKey(_ + _)
-    val urlNumUr = labelLineRddUr.map(_._1.split("\t")).map(x=>(x(0),1)).reduceByKey(_+_)
+    val urlNumUr = labelLineRddUr.map(_._1.split("\t")).map(x=>(x(0), 1)).reduceByKey(_+_)
 
 
-    /**
-      * 3. exploring the target reference
-      */
+    //3. exploring the target reference
     //3.1 label the target reference as 1 and cache the outputted records
     val labelLineRddRe = data.filter(x => totalUrl.contains(x._4)).map(x=>{
+
       val url = x._4
       val domain = url
       (x._1 + "\t" + domain, 1)
+
     }).reduceByKey(_ + _)
 
     labelLineRddRe.persist(MEMORY_AND_DISK)
 
     //3.2 calculate how many times each Ad visits the target url and how many distinct target url each Ad visits
     val visitNumRe = labelLineRddRe.map(x => (x._1.split("\t")(0), x._2)).reduceByKey(_ + _)
-    val urlNumRe = labelLineRddRe.map(_._1.split("\t")).map(x=>(x(0),1)).reduceByKey(_+_)
+    val urlNumRe = labelLineRddRe.map(_._1.split("\t")).map(x=>(x(0), 1)).reduceByKey(_+_)
 
 
-    /**
-      * 4. join the results
-      */
+    //4. join the results
     val finalTableUr = visitNumUr.join(urlNumUr)
     val finalTableRe = visitNumRe.join(urlNumRe)
-    val finalTable = finalTableUr.join(finalTableRe).sortBy(x=>x._2._1._2,false)
+    val finalTable = finalTableUr.join(finalTableRe).sortBy(x=>x._2._1._2, false)
 
 
-    /**
-      *  5.save the total result
-      */
-    finalTable.coalesce(1).saveAsTextFile("args(2)")
+    //5.save the total result
+    finalTable.coalesce(1).saveAsTextFile(args(2))
 
     data.unpersist()
     labelLineRddUr.unpersist()
