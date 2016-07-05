dbe18fd2a51a4cce78d34e02cc99567ee1003632
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-07-05 17:36:43 +0800

    + 修改部分不符合规范的地方

--- src/main/scala/com/kunyan/userportrait/importdata/crawler/CrawlerMaiMaiScheduler.scala
+++ src/main/scala/com/kunyan/userportrait/importdata/crawler/CrawlerMaiMaiScheduler.scala
@@ -3,14 +3,13 @@ package com.kunyan.userportrait.importdata.crawler
 import java.util.concurrent.{ExecutorCompletionService, Executors, Future}
 
 import com.kunyan.userportrait.config.SparkConfig
-import com.kunyan.userportrait.db.{DBOperation, Table}
 import com.kunyan.userportrait.db.Table.MaiMai
+import com.kunyan.userportrait.db.{DBOperation, Table}
 import com.kunyan.userportrait.importdata.crawler.request.MaiMaiRequest
 import com.kunyan.userportrait.importdata.crawler.task.MaiMaiSubTask
 import com.kunyan.userportrait.importdata.extractor.Extractor
 import com.kunyan.userportrait.log.PLogger
 import com.kunyan.userportrait.util.FileUtil
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable
@@ -24,17 +23,16 @@ object CrawlerMaiMaiScheduler {
 
   val sparkConf = new SparkConf()
     .setAppName("USER")
-    .set("spark.serializer",SparkConfig.SPARK_SERIALIZER)
-    .set("spark.kryoserializer.buffer.max",SparkConfig.SPARK_KRYOSERIALIZER_BUFFER_MAX)
+    .set("spark.serializer", SparkConfig.SPARK_SERIALIZER)
+    .set("spark.kryoserializer.buffer.max", SparkConfig.SPARK_KRYOSERIALIZER_BUFFER_MAX)
 
   val sc = new SparkContext(sparkConf)
-  val sqlContext = new SQLContext(sc)
   val userInfoList = new ListBuffer[String]()
-  val userMaiMaiList = new ListBuffer[(MaiMai,String)]()
-  val userMapListBuffer = new ListBuffer[mutable.HashMap[String,String]]()
-  val userKeyMap = new mutable.HashMap[String,mutable.HashMap[String,String]]()
-  val set = new mutable.HashSet[Future[mutable.HashSet[(String,mutable.HashMap[String,String])]]]()
-  val subSet = new mutable.HashSet[Future[(String,mutable.HashMap[String,String])]]()
+  val userMaiMaiList = new ListBuffer[(MaiMai, String)]()
+  val userMapListBuffer = new ListBuffer[mutable.HashMap[String, String]]()
+  val userKeyMap = new mutable.HashMap[String, mutable.HashMap[String, String]]()
+  val set = new mutable.HashSet[Future[mutable.HashSet[(String, mutable.HashMap[String, String])]]]()
+  val subSet = new mutable.HashSet[Future[(String, mutable.HashMap[String, String])]]()
 
   def main(args: Array[String]) {
 
@@ -78,20 +76,25 @@ object CrawlerMaiMaiScheduler {
       }
 
       userMapListBuffer.foreach { map =>
-        val phone = if(map.contains("手机号") && !map.get("手机号").get.contains("好友级别可见")) map.get("手机号").get else "Nodef"
+        var phone = "Nodef"
+
+        if(map.contains("手机号") && !map.get("手机号").get.contains("好友级别可见")) {
+          phone = map.get("手机号").get
+        }
+
         if(!userKeyMap.contains(phone))
           userKeyMap.put(phone, map)
       }
 
-      PLogger.warn("phone 去重完毕:"+userKeyMap.size)
+      PLogger.warn("phone 去重完毕:"+ userKeyMap.size)
 
-      val noUseMap = userKeyMap.filter(x => Table.isExistMaiMai("phone",x._1,DBOperation.connection)._1 == -1)
+      val noUseMap = userKeyMap.filter(x => Table.isExistMaiMai("phone", x._1, DBOperation.connection)._1 == -1)
 
       PLogger.warn("phone 有效数量:"+ noUseMap.size)
 
       noUseMap.foreach { user =>
           val maiMai = getMaiMainMainIndex(user._2)
-          userMaiMaiList.+=((maiMai._1,maiMai._2))
+          userMaiMaiList.+=((maiMai._1, maiMai._2))
           userInfoList.+=(maiMai._2)
       }
 
@@ -99,15 +102,16 @@ object CrawlerMaiMaiScheduler {
       val exist = userMaiMaiList.filter(x => x._1.mainIndex != -1)
       val noExist = userMaiMaiList.filter(x => x._1.mainIndex == -1).filter(x => x._1.phone != "Nodef")
 
-      val updateMain = noExist.map(x =>(x._1.phone,"",""))
-      DBOperation.batchInsert(Array("phone","qq","weibo"),updateMain)
+      val updateMain = noExist.map(x =>(x._1.phone, "", ""))
+      DBOperation.batchInsert(Array("phone", "qq", "weibo"), updateMain)
       DBOperation.maiMaiInsert(exist)
       PLogger.warn("写入数据库完毕..................:" + exist.size)
 
       val newExist = noExist.map(x => {
-        x._1.mainIndex = Table.isExist("phone",x._1.phone,DBOperation.connection)._1
+        x._1.mainIndex = Table.isExist("phone", x._1.phone, DBOperation.connection)._1
         x
       }).filter(_._1.mainIndex != -1)
+
       DBOperation.maiMaiInsert(newExist)
       PLogger.warn("写入数据库完毕..................:" + newExist.size)
 
@@ -117,7 +121,7 @@ object CrawlerMaiMaiScheduler {
 
     } finally {
 
-      FileUtil.writeToFile(args(1),userInfoList.distinct.toArray)
+      FileUtil.writeToFile(args(1), userInfoList.distinct.toArray)
       PLogger.warn("写入文件完毕..................")
       DBOperation.connection.close()
       es.shutdown()
@@ -137,26 +141,64 @@ object CrawlerMaiMaiScheduler {
 
     var info = ""
     var maiMai:MaiMai = null
-    val phone = if(map.contains("手机号") && !map.get("手机号").get.contains("提升好友级别可见")) map.get("手机号").get else "Nodef"
-    val mainIndex = Table.isExist("phone",phone,DBOperation.connection)
-    val mainIndexId = mainIndex._1
-    val email = if(map.contains("邮箱")) map.get("邮箱").get else ""
-    val job = if(map.contains("工作")) map.get("工作").get else ""
-    val position = if(map.contains("职位")) map.get("职位").get else ""
-    val realName = if(map.contains("姓名")) map.get("姓名").get else ""
-    val company = if(map.contains("工作经历")) map.get("工作经历").get else ""
-    val education = if(map.contains("教育经历")) map.get("教育经历").get else ""
-    var address = if(map.contains("地址")) map.get("地址").get else "Nodef"
+    var phone = "Nodef"
+    var email = ""
+    var job = ""
+    var position = ""
+    var realName = ""
+    var company = ""
+    var education = ""
+    var address = "Nodef"
 
-    if(address == "Nodef") {
-      address = if(map.contains("地区")) map.get("地区").get else ""
+    if(map.contains("手机号") && !map.get("手机号").get.contains("提升好友级别可见")) {
+      phone = map.get("手机号").get
     }
 
-    val maimai = MaiMai(mainIndexId,phone,email,job,position,realName,company,education,address)
-    maiMai = maimai
+    val mainIndex = Table.isExist("phone", phone, DBOperation.connection)
+    val mainIndexId = mainIndex._1
+
+    if(map.contains("邮箱")) {
+      email = map.get("邮箱").get
+    }
+
+    if(map.contains("工作")) {
+      job = map.get("工作").get
+    }
+
+    if(map.contains("职位")) {
+      position = map.get("职位").get
+    }
+
+    if(map.contains("姓名")) {
+      realName = map.get("姓名").get
+    }
+
+    if(map.contains("工作经历")) {
+      company = map.get("工作经历").get
+    }
+
+    if(map.contains("教育经历")) {
+      education  = map.get("教育经历").get
+    }
+
+    if(map.contains("地址")) {
+      address = map.get("地址").get
+    }
+
+    if(address == "Nodef") {
+
+      if(map.contains("地区")) {
+        address = map.get("地区").get
+      } else {
+        address = ""
+      }
+
+    }
+
+    maiMai =  MaiMai(mainIndexId, phone, email, job, position, realName, company, education, address)
     info = realName + "\t" + phone +  "\t" + email +  "\t" +  position +  "\t" + job +  "\t" + address +  "\t" + company +  "\t" + education
 
-    (maiMai,info)
+    (maiMai, info)
 
   }
 }
